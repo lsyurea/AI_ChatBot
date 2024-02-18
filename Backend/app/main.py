a84400d2010ccd92a94342adf5d6cc3ebe8356cb
leesiyuan <lee.si.yuan.20@gmail.com>
2024-02-18 19:05:15 +0800

    Update main.py
    Added CORS.

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from tasks import task_router
 from db import init_db
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    )
 
 @app.on_event("startup")
 async def connect():
