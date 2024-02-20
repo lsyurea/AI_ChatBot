f45ddda3cf5b3fbcf4d5ea8fde2c89d35ce083b0
leesiyuan <lee.si.yuan.20@gmail.com>
2024-02-21 03:06:14 +0800

    Fix docker bug

--- Backend/app/db.py
+++ Backend/app/db.py
@@ -6,7 +6,7 @@ import beanie
 
 async def init_db():
     # use it if you want to run python3 run.py directly or with uvicorn and docker compose up -d
-    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://db:27017")
 
     # use it if you just want to run docker compose up -d
     # client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://ai_db:27017")
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -4,13 +4,18 @@ from tasks import task_router
 from db import init_db
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
-    )
+)
 
 @app.on_event("startup")
 async def connect():
