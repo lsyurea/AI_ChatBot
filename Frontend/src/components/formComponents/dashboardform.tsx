f8e16dc7d5c70cf0899890b00f1d13cb54ba0c9f
leesiyuan <lee.si.yuan.20@gmail.com>
2024-02-21 03:45:06 +0800

    Update dashboard

--- Frontend/src/components/formComponents/dashboardform.tsx
+++ Frontend/src/components/formComponents/dashboardform.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { deleteConversation, fetchChatSummary } from "@/utils/api"
+import { useRouter } from 'next/navigation';
 import Link from "next/link"
+import { useState } from "react"
 
 interface DashboardFormProps {
     convo: {
@@ -10,15 +14,31 @@ interface DashboardFormProps {
 }
 
 export const DashboardForm = ({convo} : DashboardFormProps) => {
+    const router = useRouter();
+    const [convos, setConvos] = useState(convo);
+    async function deleteChat(id: string) {
+        router.push('/dashboard')
+        await deleteConversation(id);
+        const newConvo = await fetchChatSummary();
+        setConvos(newConvo);
+        router.push('/dashboard')
+    }
+
     return (
         <div className="w-full min-h-[300px] max-h-[600px] justify-center items-center p-10 overflow-auto border-cyan-900 border-2 rounded-xl gap-4">
             {
-                convo?.map((c) => {
+                convos?.map((c) => {
                     return (
                         <Link key={c.id} 
                             href={`/chat/${c.id}`}
-                            className="flex w-full flex-col justify-center items-center p-10 border-cyan-900 border-2 rounded-xl hover:bg-cyan-900"
+                            className="flex w-full relative flex-col justify-center items-center p-10 border-cyan-900 border-2 rounded-xl hover:bg-cyan-900"
+                            replace
                             >
+                            <div className="absolute left-0 top-0 p-2 border-red-300 border-2 rounded-tl-xl rounded-br-xl hover:bg-red-600"
+                                onClick={() => deleteChat(c.id)}
+                            >
+                                X
+                            </div>
                             <h1>{c.name}</h1>
                             <p>Number of tokens: {c.tokens}</p>
                         </Link>
--- Frontend/src/components/sidebar/sidebar.tsx
+++ Frontend/src/components/sidebar/sidebar.tsx
@@ -28,6 +28,7 @@ export function Sidebar() {
           var(--mantine-color-dark-0))] hover:bg-[light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-5))] transition-colors
           ${link === activeLink ? 'bg-[var(--mantine-color-blue-light)] color-[var(--mantine-color-blue-light-color)]' : ''}`}
         href={`/${link.toLowerCase()}`}
+        replace
       >
         {link.replace(
           /\w\S*/g,
@@ -44,7 +45,7 @@ export function Sidebar() {
       <div className={`flex flex-1`}>
 
         <div className={`flex-1 bg-[light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-6))]`}>
-          <Link href={`/`}>
+          <Link href={`/`} replace>
             <Title order={4} className="font-greycliff-cf var(--mantine-font-family) mb-8 bg-body p-4 pt-[18px] h-[60px] border-b border-solid border-gray-300 dark:border-dark-7">
               QueryAI
             </Title>
