83c2aae7f9559abfd81a130ff5e8f85dab53de5c
leesiyuan <lee.si.yuan.20@gmail.com>
2024-02-21 03:06:39 +0800

    Update api.ts

--- Frontend/src/utils/api.ts
+++ Frontend/src/utils/api.ts
@@ -1,24 +1,20 @@
-const backEndAPI: string = "http://localhost:8000";
+const backEndAPI: string = "http://localhost:80";
 
-export const fetchResponseFromUserInput = async (input: string) => {
+// This function sends a POST request to the backend API with the user's input
+export const fetchResponseFromUserInput = async (input: string, id: string) => {
     try {
-        console.log(input)
-        const res = await fetch(`${backEndAPI}/queries/`, {
+        const queryParams = new URLSearchParams({ id: id });
+        const res = await fetch(`${backEndAPI}/queries?${queryParams}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ 
-                "id": "550e8400-e29b-41d4-a716-446655440000",
-                "name": "test",
-                "params": {
-                    "param1": "value1"
-                },
-                "tokens": 100,
-                "messages": [
-                    {"role": "user", "content": input}
-                ]
+                "role": "user", 
+                "content": input
+                
             }),
+            cache: 'no-store' 
         });
 
         if (!res.ok) {
@@ -27,7 +23,138 @@ export const fetchResponseFromUserInput = async (input: string) => {
 
         const data = await res.json();
         console.log(data)
-        return data?.content || "No response";
+        return data || "No response";
+        
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+// This function retrieves the chat history from the backend API
+export const fetchChatHistory = async (id: string) => {
+    try {
+        const res = await fetch(`${backEndAPI}/conversations/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            cache: 'no-store' 
+        });
+
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        return data || "No response";
+        
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+// This function retrieves the chat summary from the backend API
+export const fetchChatSummary = async () => {
+    try {
+        const res = await fetch(`${backEndAPI}/conversations`, {
+            method: "GET",
+            cache: 'no-store' 
+        });
+
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        return data || "No response";
+        
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+// This function creates new conversation in the backend API
+export const createNewConversation = async (post: any) => {
+    try {
+        const res = await fetch(`${backEndAPI}/conversations/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(post),
+            cache: 'no-store' 
+        });
+
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        return data || "No response";
+        
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+// This function modifies the conversation in the backend API
+export const modifyConversation = async (id: string, post: any) => {
+    try {
+        const res = await fetch(`${backEndAPI}/conversations/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(post),
+            cache: 'no-store' 
+        });
+
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        return data || "No response";
+        
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+// This function deletes the conversation in the backend API
+export const deleteConversation = async (id: string) => {
+    try {
+        const res = await fetch(`${backEndAPI}/conversations/${id}`, {
+            method: "DELETE",
+            cache: 'no-store' 
+        });
+
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        return data || "No response";
+        
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+// do a status call
+export const statusCheck = async () => {
+    try {
+        const res = await fetch(`${backEndAPI}/`, {
+            method: "GET",
+            cache: 'no-store' 
+        });
+
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await res.json();
+        return data || "No response";
         
     } catch (error) {
         console.error("There was an error!", error);
